f1f2b0cff73fda4ab6558653e38376db060dd5cd
prakharsinghal311 <prakhargd31121999@gmail.com>
2023-10-15 22:55:42 +0530

    Package Catalogue Created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,174 @@
-import logo from './logo.svg';
-import './App.css';
+import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
+import DoneIcon from "@mui/icons-material/Done";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mx-20 sm:mx-32 md:mx-36 lg:mx-40 xl:mx-52 p-4 xl:p-10 xl:h-screen">
+        <div className="w-full h-full border border-1 border-gray-200 shadow-md rounded-md p-3 overflow-hidden">
+          <div className="bg-gradient-to-r from-zinc-500 to-zinc-400 justify-center w-full min-h-auto xl:h-3/5 border border-1 border-gray-200 shadow-sm rounded-md p-2 overflow-hidden">
+            <div className="text-center font-semibold text-2xl text-white">
+              Basic
+            </div>
+            <div className="text-center font-semibold text-sm text-black">
+              Package
+            </div>
+            <div className="flex justify-center flex-row items-center font-bold text-lg text-white">
+              <CurrencyRupeeIcon />
+              <div className="text-4xl">15000</div>+GST
+            </div>
+            <div className="text-center font-medium text-sm text-white">
+              /Monthly
+            </div>
+            <ol className=" grid grid-cols-1 divide-y list-inside list-disc justify-center font-medium text-sm text-white">
+              <li className="m-1">
+                Get one to two level of bank Nifty or Nifty
+              </li>
+              <li className="m-1">Get proper entry & exit on every level</li>
+              <li className="m-1">Get proper target or stopless</li>
+            </ol>
+            <div class="flex flex-col items-center">
+              <button class="bg-green-300 m-2  text-black font-bold py-2 px-4 rounded">
+                Choose Plan
+              </button>
+            </div>
+          </div>
+          <div className="text-base font-semibold">Benefit :</div>
+          <div className="flex flex-row">
+            <DoneIcon />
+            <div className="text-sm">
+              Live market customer support is available
+            </div>
+          </div>
+          <div className="flex flex-row">
+            <DoneIcon />
+            <div className="text-sm">Accuracy level up to 80%-85%</div>
+          </div>
+          <div className="flex flex-row">
+            <DoneIcon />
+            <div className="text-sm">Risk level-High to very High</div>
+          </div>
+          <div className="flex flex-row">
+            <DoneIcon />
+            <div className="text-sm">Priority customer service</div>
+          </div>
+        </div>
+        <div className="w-full h-full border border-1 border-gray-200 shadow-md rounded-md p-3 overflow-hidden">
+          <div className="bg-gradient-to-r from-emerald-500 to-emerald-900 justify-center w-full min-h-auto xl:h-3/5 border border-1 border-gray-200 shadow-sm rounded-md p-2 overflow-hidden">
+            <div className="text-center font-semibold text-2xl text-white">
+              Premium
+            </div>
+            <div className="text-center font-semibold text-sm text-black">
+              Package
+            </div>
+            <div className="flex justify-center flex-row items-center font-bold text-lg text-white">
+              <CurrencyRupeeIcon />
+              <div className="text-4xl">35000</div>+GST
+            </div>
+            <div className="text-center font-medium text-sm text-white">
+              /Monthly
+            </div>
+            <ol className=" grid grid-cols-1 divide-y list-inside list-disc justify-center font-medium text-sm text-white">
+              <li className="m-1">
+                Get one to three level of bank Nifty or Nifty
+              </li>
+              <li className="m-1">Proper entry & stopless</li>
+              <li className="m-1">Get BIST traders</li>
+            </ol>
+            <div class="flex flex-col items-center">
+              <button class="bg-green-300 m-2  text-black font-bold py-2 px-4 rounded">
+                Choose Plan
+              </button>
+            </div>
+          </div>
+          <div className="text-base font-semibold">Benefit :</div>
+          <div className="flex flex-row">
+            <DoneIcon />
+            <div className="text-sm">Two Targets</div>
+          </div>
+          <div className="flex flex-row">
+            <DoneIcon />
+            <div className="text-sm">
+              Live market customer support is available
+            </div>
+          </div>
+          <div className="flex flex-row">
+            <DoneIcon />
+            <div className="text-sm">Accuracy level up to 80%-85%</div>
+          </div>
+          <div className="flex flex-row">
+            <DoneIcon />
+            <div className="text-sm">Risk level-High to very High</div>
+          </div>
+          <div className="flex flex-row">
+            <DoneIcon />
+            <div className="text-sm">Priority customer service</div>
+          </div>
+        </div>
+        <div className="w-full h-full border border-1 border-gray-200 shadow-md rounded-md p-3 overflow-hidden">
+          <div className="bg-gradient-to-r from-zinc-500 to-zinc-400 justify-center w-full min-h-auto xl:h-3/5 border border-1 border-gray-200 shadow-sm rounded-md p-2 overflow-hidden">
+            <div className="text-center font-semibold text-2xl text-white">
+              Ultra Premium
+            </div>
+            <div className="text-center font-semibold text-sm text-black">
+              Package
+            </div>
+            <div className="flex justify-center flex-row items-center font-bold text-lg text-white">
+              <CurrencyRupeeIcon />
+              <div className="text-4xl">75000</div>+GST
+            </div>
+            <div className="text-center font-medium text-sm text-white">
+              /Monthly
+            </div>
+            <ol className=" grid grid-cols-1 divide-y list-inside list-disc justify-center font-medium text-sm text-white">
+              <li className="m-1">
+                Get one to four level of bank Nifty or Nifty
+              </li>
+              <li className="m-1">Get proper entry & exit</li>
+              <li className="m-1">
+                Get well researched targets and stopless on every level
+              </li>
+            </ol>
+            <div class="flex flex-col items-center">
+              <button class="bg-green-300 m-2  text-black font-bold py-2 px-4 rounded">
+                Choose Plan
+              </button>
+            </div>
+          </div>
+          <div className="text-base font-semibold">Benefit :</div>
+          <div className="flex flex-row">
+            <DoneIcon />
+            <div className="text-sm">
+              Get expiry special level on every weekly expiry
+            </div>
+          </div>
+          <div className="flex flex-row">
+            <DoneIcon />
+            <div className="text-sm">Get BTST traders</div>
+          </div>
+          <div className="flex flex-row">
+            <DoneIcon />
+            <div className="text-sm">
+              Get one-two level provide finnifty every week
+            </div>
+          </div>
+          <div className="flex flex-row">
+            <DoneIcon />
+            <div className="text-sm">
+              Live market customer support is available
+            </div>
+          </div>
+          <div className="flex flex-row">
+            <DoneIcon />
+            <div className="text-sm">Accuracy level up to 80%-85%</div>
+          </div>
+          <div className="flex flex-row">
+            <DoneIcon />
+            <div className="text-sm">Risk level-High to very High</div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
